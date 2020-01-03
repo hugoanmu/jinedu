a551c921cbb4ad5acbddfc6a512fe58cc5528338
HEO <ultra6000@hanmail.net>
2020-01-03 16:50:45 +0900

    호스트변경

--- data/dbconfig.php
+++ data/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 if (!defined('_GNUBOARD_')) exit;
-define('G5_MYSQL_HOST', 'localhost');
+define('G5_MYSQL_HOST', 'ultra6000.cafe24.com');
 define('G5_MYSQL_USER', 'ultra6000');
 define('G5_MYSQL_PASSWORD', 'wls62628790');
-define('G5_MYSQL_DB', 'jinedu');
+define('G5_MYSQL_DB', 'ultra6000');
 define('G5_MYSQL_SET_MODE', true);
 
 define('G5_TABLE_PREFIX', 'g5_');
